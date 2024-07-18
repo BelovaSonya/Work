32bee2180ccd1190af90499aa48da7dc00932c54
BelovaSonya <sofibella-1@mail.ru>
2024-07-18 09:58:56 +0300

    Test commit

---
+++ UserClass.js
@@ -0,0 +1,42 @@
+'use strist'
+
+class User {
+    #login;
+    #_password;
+
+    constructor(login, password){
+        this.#login = login;
+        this.#password = password;
+    }
+    set #password(pass){
+        this.#_password = pass.split('').reverse().join('');
+    }
+
+    get #password(){
+        return this.#_password.split('').reverse().join('');
+    }
+
+    get login(){
+        return this.#login;
+    }
+
+    checkPassword(pass){
+        return this.#password === pass;
+    }
+
+    changePassword(oldPass, newPass){
+        if (this.changePassword(oldPass)){
+            return false;
+        }
+        this.#password = newPass;
+        return true;
+    }
+}
+
+const user = new User('a@a.ru','123');
+console.log(user);
+console.log(user.login);
+console.log(user.checkPassword('223'));
+console.log(user.checkPassword('123'));
+console.log(user.changePassword('123','223'));
+console.log(user);
