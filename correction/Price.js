14a32bee7171a6c799deca1c02b18e00a0d4df35
BelovaSonya <sofibella-1@mail.ru>
2024-07-29 09:14:27 +0300

    Commit commit

---
+++ correction/Price.js
@@ -0,0 +1,13 @@
+'use strict'
+
+/* получить среднюю цену 30 товаров из API */
+
+const request = new XMLHttpRequest();
+request.open('GET' , 'https://dummyjson.com/products');
+request.send();
+
+request.addEventListener('load' , function(){
+    const { products } = JSON.parse(this.responseText);
+    const sum = products.reduce((acc, p) => acc += p.price, 0);
+    console.log(sum / products.length);
+})
